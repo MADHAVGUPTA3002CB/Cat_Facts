ab21b138a02425c740d89a7eb7badb752288c278
madhav <guptamadhav137@gmail.com>
2023-04-28 00:26:33 +0530

    add padding and margin

--- app/src/main/java/com/example/cat_facts/MainActivity.kt
+++ app/src/main/java/com/example/cat_facts/MainActivity.kt
@@ -2,34 +2,23 @@ package com.example.cat_facts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
-import org.json.JSONObject
-import java.lang.Math.random
 import kotlin.random.Random
-import kotlin.random.nextInt
+
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var refresh_button : Button
+
     var data : JSONArray? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         loadfact()
-//        refresh_button.setOnClickListener {
-//            loadfact()
-//        }
-
     }
 
     private fun loadfact(){
@@ -41,37 +30,16 @@ class MainActivity : AppCompatActivity() {
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
-
-
                 data = response.getJSONArray("data")
-//                var length : Int
-//                data?.let {
-//                    length = data!!.length()
-//                }
-//                val length : Int? = data.length()
                 val randomNumber = Random.nextInt(data!!.length())
                 val jsonObject = data!!.getJSONObject(randomNumber)
                 val fact = jsonObject.getString("fact")
-
-
-
-//                Log.d("facts" , "$data")
-//                val jsonArray = data.optJSONArray("fact")
-
-
-//                data = response.getJSONObject("data")
                 val fact_view : TextView = findViewById(R.id.fact_text)
                 fact_view.text = fact
-
-
-
-
             },
             {
-                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(this, "Something went wrong \n ${it.localizedMessage}", Toast.LENGTH_SHORT).show()
             })
-
         queue.add(jsonObjectRequest)
         }
     fun nextfact(view: View) {
